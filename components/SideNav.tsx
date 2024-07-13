6b231c5f9c988cd5bd3dc4f2624c620ab484b5f1
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-13 22:14:00 +0530

    feat: Add NextThemesProvider to ThemeProvider component

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,13 +1,20 @@
 import Logo from "./Logo";
+import MoreDropdown from "./MoreDropdown";
+import Navlinks from "./Navlinks";
 
 function SideNav() {
   return (
-    <div>
-      <div className="">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div className="border-t -ml-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 w-full md:relative md:h-full bottom-0 md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2 align-left">
         <Logo />
-        {/* <NavLinks /> */}
+        <Navlinks />
         {/* user && <ProfileLink/> */}
-        <div>{/* More Dropdown */}</div>
+        <div
+          className="hidden md:flex relative md:mt-auto flex-1 items-end
+        w-full"
+        >
+          <MoreDropdown />
+        </div>
       </div>
     </div>
   );
