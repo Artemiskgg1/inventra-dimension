9a966b1d2f0e069273bc95a691b1c046ac161a80
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-14 00:15:30 +0530

    chore: Update .gitignore and package.json dependencies

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import { config } from "@/auth";
+import NextAuth from "next-auth/next";
+const handler = NextAuth(config);
+
+export { handler as GET, handler as POST };
--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,18 +1,21 @@
+// import { auth } from "@/auth";
 import Logo from "./Logo";
 import MoreDropdown from "./MoreDropdown";
-import Navlinks from "./Navlinks";
+// import ProfileLink from "./ProfileLink";
+import NavLinks from "./Navlinks";
 
-function SideNav() {
+async function SideNav() {
+  // const session = await auth();
+  // const user = session?.user;
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <div className="border-t -ml-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 w-full md:relative md:h-full bottom-0 md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2 align-left">
+      <div className="border-t -ml-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 w-full md:relative md:h-full bottom-0 md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2">
         <Logo />
-        <Navlinks />
-        {/* user && <ProfileLink/> */}
-        <div
-          className="hidden md:flex relative md:mt-auto flex-1 items-end
-        w-full"
-        >
+        <NavLinks />
+        {/* {user && <ProfileLink user={user} />} */}
+
+        <div className="hidden md:flex relative md:mt-auto flex-1 items-end w-full">
           <MoreDropdown />
         </div>
       </div>
